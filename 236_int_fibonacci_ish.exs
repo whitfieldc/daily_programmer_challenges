5ff4bbd738db10504d82bfb247d3e69e2409e5fa
Charles <whitfield.cw@gmail.com>
2015-10-18 18:42:32 -0700

    add passing solution

--- 236_int_fibonacci_ish.exs
+++ 236_int_fibonacci_ish.exs
@@ -26,31 +26,49 @@ defmodule FibonacciIsh do
   def is_fibonacci?(candidate) do
     multiplied = 5*candidate*candidate
     plus = :math.sqrt(multiplied + 4)
-    minus = :math.sqrt(multiplied - 4)
+    minus = try do
+              :math.sqrt(multiplied - 4)
+            rescue
+              ArithmeticError -> false
+            end
     (plus == round(plus)) || (minus == round(minus))
   end
 
-  def determine_fib_factor_and_counterpart(num) do
+  def get_fib_ish_sequence(num) do
     if is_fibonacci?(num) do
-      [1, num]
+      build_fib_ish_string(1, num)
     else
       counterpart = find_factors(num)
-      |> Enum.map(fn(x) -> Kernel.trunc(num/x) end)
+      |> Enum.flat_map(fn(x) -> [x, Kernel.trunc(num/x)] end)
+      # |> Enum.concat(Enum.map(fn(x) -> [x, Kernel.trunc(num/x)] end))
       |> Enum.find(num, fn(x) -> is_fibonacci?(x)end)
       if counterpart == num do
-        counterpart
+        build_fib_ish_string(counterpart, counterpart)
       else
-        [Kernel.trunc(num/counterpart), counterpart]
+       build_fib_ish_string(Kernel.trunc(num/counterpart), num)
       end
     end
   end
 
-  # def print_fib_ish_string(num) do
-
-  # end
+  def build_fib_ish_string(start, target, previous_list \\ [0]) do
+    cond do
+      hd(previous_list) == target ->
+        previous_list
+        |> Enum.reduce("", fn(x, acc) -> to_string(x) <> " " <> acc end)
+        |> IO.puts
+      hd(previous_list) == 0 ->
+        build_fib_ish_string(start, target, [start|previous_list])
+      true ->
+        [first | rest] = previous_list
+        build_fib_ish_string(start, target, [(first + hd(rest))|previous_list])
+    end
+  end
 
 end
 
-# FibonacciIsh.get_range_from_one_to_sqrt(9)
-FibonacciIsh.determine_fib_factor_and_counterpart(21)
-FibonacciIsh.determine_fib_factor_and_counterpart(84)
+FibonacciIsh.get_fib_ish_sequence(0)
+IO.puts("---------------------------")
+FibonacciIsh.get_fib_ish_sequence(578)
+IO.puts("---------------------------")
+FibonacciIsh.get_fib_ish_sequence(123456789)
+IO.puts("---------------------------")
