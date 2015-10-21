62d7f57e7e784cd05ab19fcebec367fce5a8ae49
Charles <whitfield.cw@gmail.com>
2015-10-20 21:09:45 -0700

    add broken map creation

--- 237_easy_broken_keyboard.exs
+++ 237_easy_broken_keyboard.exs
@@ -1,7 +1,7 @@
-case File.read("237_easy_dictionary.txt") do
-  {:ok, body} ->  IO.puts(body)
-  {:error, reason} -> IO.puts reason
-end
+# case File.read("237_easy_dictionary.txt") do
+#   {:ok, body} ->  IO.puts(body)
+#   {:error, reason} -> IO.puts reason
+# end
 
 ## split string at each newline or recursively split and categorize?
 
@@ -9,3 +9,29 @@ end
 ## make that string an :atom, which will be key for the current longest word of that consituent unique letter set
 ## store :atoms in dictionary or map (find out what diff is?)
 ## break down input words same way and look em up by their unique :atom
+defmodule BrokenKeyboard do
+
+  def build_longest_word_by_unique_letters_map(dictionary_string) do
+
+  end
+
+  def atomize_sorted_uniques(word) do
+    word
+    |> String.split("")
+    |> Enum.uniq
+    |> Enum.sort
+    |> Enum.join
+    |> String.to_atom
+    |> IO.inspect
+  end
+
+  def add_to_map_or_discard(atom_key, candidate, master_map) do
+    unless String.length(master_map[atom_key]) > String.length(candidate) do
+      new_master_map = master_map
+      |> Map.put_new(atom_key, candidate)
+      |> IO.inspect
+    end
+    new_master_map || master_map
+  end
+
+end
